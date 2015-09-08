46b7fc51e74ca31f711731b436b3fe92b19609a1
Chris Sauve <chris.sauve@shopify.com>
2015-09-08 10:52:01 -0400

    Updated React rules

--- rules/react.js
+++ rules/react.js
@@ -1,19 +1,53 @@
 module.exports = {
+  // Prevent missing displayName in a React component definition
   "react/display-name": [2, { acceptTranspilerName: true }],
+  // Enforce boolean attributes notation in JSX
   "react/jsx-boolean-value": 1,
-  "react/jsx-no-undef": 1,
-  "react/jsx-quotes": 1,
-  "react/jsx-sort-prop-types": 1,
+  // Validate closing bracket location in JSX
+  "react/jsx-closing-bracket-location": [1, { location: "tag-aligned" }],
+  // Enforce or disallow spaces inside of curly braces in JSX attributes
+  "react/jsx-curly-spacing": [1, "never", { allowMultiline: true }],
+  // Validate props indentation in JSX
+  "react/jsx-indent-props": [2, 2],
+  // Limit maximum of props on a single line in JSX
+  "react/jsx-max-props-per-line": 0,
+  // Prevent duplicate props in JSX
+  "react/jsx-no-duplicate-props": 2,
+  // Prevent usage of unwrapped JSX strings
+  "react/jsx-no-literals": 0,
+  // Disallow undeclared variables in JSX
+  "react/jsx-no-undef": 2,
+  // Enforce quote style for JSX attributes
+  "react/jsx-quotes": [1, "single", "avoid-escape"],
+  // Enforce propTypes declarations alphabetical sorting
+  "react/jsx-sort-prop-types": 0,
+  // Enforce props alphabetical sorting
   "react/jsx-sort-props": 0,
-  "react/jsx-uses-react": 1,
-  "react/jsx-uses-vars": 1,
+  // Prevent React to be incorrectly marked as unused
+  "react/jsx-uses-react": 0,
+  // Prevent variables used in JSX to be incorrectly marked as unused
+  "react/jsx-uses-vars": 2,
+  // Prevent usage of dangerous JSX properties
+  "react/no-danger": 2,
+  // Prevent usage of setState in componentDidMount
   "react/no-did-mount-set-state": 1,
+  // Prevent usage of setState in componentDidUpdate
   "react/no-did-update-set-state": 1,
+  // Prevent multiple component definition per file
   "react/no-multi-comp": 0,
-  "react/no-unknown-property": 1,
+  // Prevent usage of setState
+  "react/no-set-state": 0,
+  // Prevent usage of unknown DOM property
+  "react/no-unknown-property": 0,
+  // Prevent missing props validation in a React component definition
   "react/prop-types": 1,
-  "react/react-in-jsx-scope": 1,
+  // Prevent missing React when using JSX
+  "react/react-in-jsx-scope": 2,
+  // Restrict file extensions that may be required
+  "react/require-extension": [2, { extensions: [".js"] }],
+  // Prevent extra closing tags for components without children
   "react/self-closing-comp": 1,
+  // Enforce component methods order
   "react/sort-comp": [
     1,
     {
@@ -26,5 +60,6 @@ module.exports = {
       ]
     }
   ],
-  "react/wrap-multilines": 1
+  // Prevent missing parentheses around multilines JSX
+  "react/wrap-multilines": 2
 }
