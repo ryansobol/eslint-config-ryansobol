d62b06cc09048a19c60e6d4f7dd3f047b60cb22d
Chris Sauve <chris.sauve@shopify.com>
2015-09-18 10:01:33 -0400

    Allow function use before definition

--- rules/variables.js
+++ rules/variables.js
@@ -20,5 +20,5 @@ module.exports = {
   // Disallow declaration of variables that are not used in the code
   "no-unused-vars": 2,
   // Disallow use of variables before they are defined
-  "no-use-before-define": 2
+  "no-use-before-define": [2, "nofunc"]
 };
