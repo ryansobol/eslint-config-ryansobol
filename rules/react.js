73e6dca39950435e21869b7e95067d6c18c3d81e
Chris Sauve <chris.sauve@shopify.com>
2016-01-06 19:58:38 -0500

    Update React plugin to 3.14

--- rules/react.js
+++ rules/react.js
@@ -11,6 +11,8 @@ module.exports = {
   'react/jsx-curly-spacing': [1, 'never', {allowMultiline: true}],
   // Validate props indentation in JSX
   'react/jsx-indent-props': [1, 2],
+  // Validate JSX indentation
+  'react/jsx-indent': [1, 2],
   // Validate JSX has key prop when in array or iterator
   'react/jsx-key': 2,
   // Limit maximum of props on a single line in JSX
@@ -35,16 +37,22 @@ module.exports = {
   'react/jsx-uses-vars': 2,
   // Prevent usage of dangerous JSX properties
   'react/no-danger': 1,
+  // Prevent usage of deprecated methods
+  'react/no-deprecated': 2,
   // Prevent usage of setState in componentDidMount
   'react/no-did-mount-set-state': 2,
   // Prevent usage of setState in componentDidUpdate
   'react/no-did-update-set-state': 2,
   // Prevent direct mutation of this.state
   'react/no-direct-mutation-state': 2,
+  // Prevent usage of isMounted
+  'react/no-is-mounted': 2,
   // Prevent multiple component definition per file
   'react/no-multi-comp': 0,
   // Prevent usage of setState
   'react/no-set-state': 0,
+  // Prevent using string references in ref attribute.
+  'react/no-string-refs': 1,
   // Prevent usage of unknown DOM property
   'react/no-unknown-property': 0,
   // Prevent missing props validation in a React component definition
